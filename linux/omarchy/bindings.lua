87e51a6c21b1d4f8ac4ea5b2072ecf9936fee904
taxin <186607085+taxin-404@users.noreply.github.com>
2026-07-23 01:25:04 +0600

    Create bindings.lua

---
+++ linux/omarchy/bindings.lua
@@ -0,0 +1,14 @@
+hl.unbind("SUPER + TAB")
+hl.bind("SUPER + TAB", function()
+    hl.plugin.scrolloverview.overview("toggle")
+end)
+
+o.bind("SUPER + Q", "Open note", "omawrite ~/note.md")
+hl.window_rule({
+  name = "note-window",
+  match = { class = "omawrite" },
+  float = true,
+  pin = true,
+  size = "600 400",
+  move = "63 400",
+})
